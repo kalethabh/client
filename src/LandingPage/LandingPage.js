5bcd404429aa8a74254f91c72c6afff27d447934
kalethabh <kalethabh@gmail.com>
2023-06-07 19:51:03 -0500

    Ya

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "../redux/actions";
@@ -8,9 +8,15 @@ import logo from "../img/ISOLOGO_HENRY_BLACK.png";
 export default function LandingPage() {
   const dispatch = useDispatch();
   const listenerPokemons = useSelector((state) => state.allPokemons);
+  const [isLoading, setIsLoading] = useState(true); // New isLoading state
 
   useEffect(() => {
-    dispatch(getPokemons());
+    const fetchPokemons = async () => {
+      await dispatch(getPokemons());
+      setIsLoading(false);
+    };
+
+    fetchPokemons();
   }, [listenerPokemons, dispatch]);
 
   return (
@@ -18,9 +24,13 @@ export default function LandingPage() {
       <h1 className="landing-page-title">Pokemon API</h1>
       <img src={logo} alt="logo" className="logo" />
       <h2 className="k">kalethabh</h2>
-      <Link to="/home">
-        <button className="landing-page-button">Ingresar</button>
-      </Link>
+      {isLoading ? ( // Conditional rendering based on isLoading state
+        <p>Loading...</p>
+      ) : (
+        <Link to="/home">
+          <button className="landing-page-button">Ingresar</button>
+        </Link>
+      )}
     </div>
   );
 }
